1c0747c1429b938a72fd5728b00b4e55c1aaca8e
NgunyiGachie <antogachie@gmail.com>
2024-04-23 09:12:31 +0300

    Complete lab

---
+++ src/components/TestComponentComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function TestComponent() {
+    return (
+      <div className="video">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/5TbUxGZtwGI"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          title="time video"
+          allowFullScreen
+        ></iframe>
+      </div>
+    );
+  }
+
+export default TestComponentComponent;
